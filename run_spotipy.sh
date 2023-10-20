1221c038a75134be5e58ae7f5aefd48656431662
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-10-20 21:21:14 +0100

    added shell script to run spotipy globally

---
+++ run_spotipy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change the directiory to the project directory
+cd ~/Desktop/computer_science/spotipy
+
+# Set the application name
+export APP_NAME="Spotipy"
+
+# Run Python script
+/bin/python3 main.py
